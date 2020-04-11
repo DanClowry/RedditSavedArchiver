431c31e17bc823917304f7055284fdbe08162f31
Daniel Clowry <daniel.clowry2001@gmail.com>
2020-04-11 19:03:37 +1000

    imp: Add Reddit client builder

---
+++ src/RedditArchiver/Account.cs
@@ -0,0 +1,18 @@
+﻿using Reddit;
+using Reddit.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RedditArchiver
+{
+    public class Account
+    {
+        private RedditClient _client;
+
+        public Account(string appId, string accessToken, string refreshToken, string userAgent)
+        {
+            _client = new RedditClient(appId: appId, accessToken: accessToken,
+                refreshToken: refreshToken, userAgent: userAgent);
+        }
+    }
+}
--- src/RedditArchiver/Program.cs
+++ src/RedditArchiver/Program.cs
@@ -4,9 +4,21 @@ namespace RedditArchiver
 {
     class Program
     {
+        private static Account _account;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _account = ConfigureAccount();
+        }
+
+        private static Account ConfigureAccount()
+        {
+            string appId = Environment.GetEnvironmentVariable("REDDIT_ARCHIVER_APPID");
+            string accessToken = Environment.GetEnvironmentVariable("REDDIT_ARCHIVER_ACCESSTOKEN");
+            string refreshToken = Environment.GetEnvironmentVariable("REDDIT_ARCHIVER_REFRESHTOKEN");
+            string userAgent = Environment.GetEnvironmentVariable("REDDIT_ARCHIVER_USERAGENT");
+
+            return new Account(appId, accessToken, refreshToken, userAgent);
         }
     }
 }
