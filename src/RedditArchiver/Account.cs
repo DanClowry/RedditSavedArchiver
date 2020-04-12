b0fa14e6c45096020e03dd995691a027096d11bf
Daniel Clowry <daniel.clowry2001@gmail.com>
2020-04-12 21:39:48 +1000

    imp(Account): Add saved posts slicing

--- src/RedditArchiver/Account.cs
+++ src/RedditArchiver/Account.cs
@@ -35,5 +35,32 @@ namespace RedditArchiver
 
             return savedPosts;
         }
+
+        /// <summary>
+        /// Gets all the posts the user has saved after another post.
+        /// </summary>
+        /// <param name="beforeName">The fullname of the post to use as an anchor point (exclusive). Will only return posts saved later than the anchor post.</param>
+        /// <remarks>Fetches all the user's saved posts if the anchor point is not one of the most recent 100 saved posts.
+        /// The post used as an anchor point is not included in the returned list of posts.</remarks>
+        /// <returns>A list of posts the user has saved. Returns an empty list if the anchor point does not exist in the user's saved posts.</returns>
+        public List<Post> GetSavedPostsBefore(string beforeName)
+        {
+            List<Post> savedPosts = _client.Account.Me.GetPostHistory("saved", limit: 100, before: beforeName);
+
+            if (savedPosts.Count < 100)
+            {
+                return savedPosts;
+            }
+
+            // Manually search the saved posts list because the Reddit API doesn't properly work if the
+            // anchor point is not in the most recent 100 saved posts.
+            savedPosts = GetAllSavedPosts();
+            int anchorIndex = savedPosts.FindIndex(p => p.Fullname == beforeName);
+            if (anchorIndex == -1)
+            {
+                return new List<Post>();
+            }
+            return savedPosts.Take(anchorIndex).ToList();
+        }
     }
 }
