5561a06c9f00504117c3c1f432361dd5441d2d89
Daniel Clowry <daniel.clowry2001@gmail.com>
2020-04-23 21:00:15 +1000

    fix(Archiver): Catch unhandled exceptions when interacting with Reddit

--- src/RedditArchiver/Archiver.cs
+++ src/RedditArchiver/Archiver.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Options;
 using Reddit.Controllers;
+using Reddit.Exceptions;
 using RedditArchiver.Data;
 using RedditArchiver.Models;
 using RedditArchiver.Services;
@@ -32,23 +33,38 @@ namespace RedditArchiver
         {
             var stopwatch = new Stopwatch();
             stopwatch.Start();
-
-            PostDTO latestSaved = await _database.GetLatestSavedPostAsync();
-            List<Post> posts = new List<Post>();
-            if (latestSaved != null)
-            {
-                posts = _userAccount.GetSavedPostsBefore(latestSaved.Fullname);
-            }
-            else
-            {
-                posts = _userAccount.GetAllSavedPosts();
-            }
-            posts.Reverse();
+            List<Post> posts = await GetPostsFromReddit();
             stopwatch.Stop();
             Console.WriteLine($"Got {posts.Count} newly saved posts from Reddit API. Took {stopwatch.Elapsed.TotalSeconds} seconds.");
             stopwatch.Restart();
 
             await ArchivePosts(posts);
+        }
+
+        private async Task<List<Post>> GetPostsFromReddit()
+        {
+            PostDTO latestSaved = await _database.GetLatestSavedPostAsync();
+            List<Post> posts = new List<Post>();
+            try
+            {
+                if (latestSaved != null)
+                {
+                    posts = _userAccount.GetSavedPostsBefore(latestSaved.Fullname);
+                }
+                else
+                {
+                    posts = _userAccount.GetAllSavedPosts();
+                }
+            }
+            catch (RedditUnauthorizedException)
+            {
+                Console.WriteLine("Got unauthorised response from Reddit when attempting to retrieve saved posts. " +
+                    "Check that the app has been authorised to access your account (reddit.com/prefs/apps/) and " +
+                    "that your user credentials in appsettings.json are correct.");
+                Environment.Exit(1);
+            }
+            posts.Reverse();
+            return posts;
         }
 
         private async Task ArchivePosts(List<Post> posts)
@@ -58,22 +74,39 @@ namespace RedditArchiver
             if (_botAccount != null)
             {
                 string crosspostSub = _settings.Crosspost.CrosspostSubreddit;
-                if (_botAccount.ModeratesSubreddit(crosspostSub))
+                if (String.IsNullOrWhiteSpace(crosspostSub))
                 {
-                    foreach (var post in posts)
-                    {
-                        List<Task> tasks = new List<Task>();
-                        tasks.Add(_botAccount.SubmitCrosspostAsync(post, crosspostSub));
-                        tasks.Add(_database.SavePostAsync(post));
-                        await Task.WhenAll(tasks);
-                        Console.WriteLine($"\"{post.Title}\" saved to database and crossposted to /r/{_settings.Crosspost.CrosspostSubreddit}");
-                    }
-                    stopwatch.Stop();
-                    Console.WriteLine($"\nSaved and crossposted {posts.Count} posts in {stopwatch.Elapsed.TotalSeconds:N0} seconds");
+                    Console.WriteLine("Crosspost subreddit must not be empty when crossposting is enabled.\n" +
+                        "Please set the CrosspostSubreddit value in your appsettings.json.");
+                    Environment.Exit(1);
                 }
-                else
+
+                try
                 {
-                    Console.WriteLine("Bot account must be a moderator of the crosspost subreddit.");
+                    if (_botAccount.ModeratesSubreddit(crosspostSub))
+                    {
+                        foreach (var post in posts)
+                        {
+                            List<Task> tasks = new List<Task>();
+                            tasks.Add(_botAccount.SubmitCrosspostAsync(post, crosspostSub));
+                            tasks.Add(_database.SavePostAsync(post));
+                            await Task.WhenAll(tasks);
+                            Console.WriteLine($"\"{post.Title}\" saved to database and crossposted to /r/{_settings.Crosspost.CrosspostSubreddit}");
+                        }
+                        stopwatch.Stop();
+                        Console.WriteLine($"\nSaved and crossposted {posts.Count} posts in {stopwatch.Elapsed.TotalSeconds:N0} seconds");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bot account must be a moderator of the crosspost subreddit.");
+                    }
+                }
+                catch (RedditUnauthorizedException)
+                {
+                    Console.WriteLine("Got unauthorised response from Reddit when attempting to crosspost. " +
+                    "Check that the app has been authorised to access the bot account (reddit.com/prefs/apps/) and " +
+                    "that the bot credentials in appsettings.json are correct.");
+                    Environment.Exit(1);
                 }
             }
             else
